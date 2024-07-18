1889335e0ce5d1a671a8ab4c00946a0020b9c8ad
lenovo <lenovo@LAPTOP-TJ9CPRNC.bwrouter>
2024-07-18 17:04:04 +0530

    First Commit

---
+++ src/firstone/Firstone.java
@@ -0,0 +1,14 @@
+package firstone;
+
+public class Firstone {
+
+	public static void main(String[] args) {
+		String name = "Shital";
+		int length = name.length();
+		String rev = "";
+			for(int i= length-1; i>=0; i--) {
+			rev = rev + name.charAt(i);
+	   }
+	System.out.println("Reverse of "+name+" is "+rev);
+	}
+}
